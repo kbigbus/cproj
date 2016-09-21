880c99555afdfb305d13e3b1412c4e26684b2033
kongwenhua <kongwenhua@yaochufa.com>
2016-09-21 15:59:35 +0800

    修改

--- les1/stdioe.c
+++ les1/stdioe.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+//Êä³ö1 ÊäÈë< ´íÎó2
 int main()
 {
-    char* inputs;
+    int a;
 
     fprintf(stdout, "standard output\n");  // printf("standard output\n")
 
-    fprintf(stdout, "input some string:\n"); // printf("input some string:\n\n")
-    fscanf(stdin, "%s", inputs); // scanf("%s", &input)
+    fprintf(stdout, "input a number:\n"); // printf("input some string:\n\n")
+    fscanf(stdin, "%d", &a); // scanf("%s", &input)
 
-    fprintf(stdout, "the string you input is : %s\n", inputs);
+    fprintf(stdout, "the number you input is : %d\n", a);
     return 0;
 }
